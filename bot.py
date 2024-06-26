5e1567c207f547642565a1647e240ed8d7bd7e8b
GNYAR <s901121s901121@gmail.com>
2024-06-26 22:04:15 +0800

    feat(tts): correct the variable to guild level

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix="/", intents=intents)
 
-channel = None
+guild_channels = {}
 
 
 @bot.event
@@ -17,8 +17,12 @@ async def on_ready():
 
 @bot.event
 async def on_message(msg):
-    global channel
-    if msg.channel == channel and msg.author != bot.user:
+    if msg.guild is None:
+        return
+
+    global guild_channels
+    channel_id = guild_channels.get(msg.guild.id)
+    if msg.channel.id == channel_id and msg.author != bot.user:
         name = msg.author.nick or msg.author.global_name or msg.author.name
         print(name, msg.content)
 
@@ -27,9 +31,9 @@ async def on_message(msg):
 
 @bot.command()
 async def set(ctx):
-    global channel
-    channel = bot.get_channel(ctx.channel.id)
-    await channel.send(f"set channel ({ctx.channel.name})")
+    global guild_channels
+    guild_channels[ctx.guild.id] = ctx.channel.id
+    await ctx.send(f"set channel ({ctx.channel.name})")
 
 
 bot.run(TOKEN)
